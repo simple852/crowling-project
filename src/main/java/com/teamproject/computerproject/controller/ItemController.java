6af4e29bc12ba41213172d785b2c08c80347b672
Administrator <sbm0709@naver.com>
2023-12-01 13:34:21 +0900

    정영식 수정사항 2023-12-01

---
+++ src/main/java/com/teamproject/computerproject/controller/ItemController.java
@@ -0,0 +1,28 @@
+package com.teamproject.computerproject.controller;
+
+
+import com.teamproject.computerproject.dto.ItemDto;
+import com.teamproject.computerproject.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/save")
+public class ItemController {
+    private final ItemService itemService;
+
+
+    @PostMapping("/item")
+    public void saveItem(ItemDto itemDto){
+
+//        itemService
+    }
+
+
+
+}
